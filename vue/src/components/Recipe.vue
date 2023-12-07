0a36804a83275a9e2379ec3d19a7546ce2b8f2f6
Leah <leahchristine21@gmail.com>
2023-12-07 11:40:53 -0500

    added some css styling

--- vue/src/components/Recipe.vue
+++ vue/src/components/Recipe.vue
@@ -85,3 +85,70 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.list-all-recipes {
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    text-align: center; /* Center text for better appearance */
+  }
+
+  h1 {
+    text-align: center;
+  }
+
+  .container {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+    gap: 1.25rem;
+  }
+
+  recipe {
+    background-color: pink;
+    border-radius: 0.625rem; /* Rounded corners for recipe cards */
+    box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.1);
+    margin-bottom: 1.25rem;
+    padding: 2%;
+  }
+
+  .add-recipe-button {
+    margin-top: 1.25rem; 
+    align-items: center;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    max-width: 18.75rem;
+    margin: auto;
+    margin-top: 1.25rem; /* Add spacing between the button and the form */
+  }
+
+  form div {
+    margin-bottom: 0.625rem;
+  }
+
+  label {
+    display: block;
+    margin-bottom: 0.3125rem;
+  }
+
+  input {
+    width: 100%;
+    padding: 0.625rem;
+    box-sizing: border-box;
+  }
+
+  button {
+    padding: 1.25rem;
+    background-color: #4caf50;
+    color: #fff;
+    border: none;
+    border-radius: 0.25rem;
+    cursor: pointer;
+  }
+
+  button:hover {
+    opacity: 0.8;
+  }
+
+</style>
