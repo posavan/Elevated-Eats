a827b24b78aba9c691f48731e03c090a37635f5f
Leah <leahchristine21@gmail.com>
2023-12-07 16:38:56 -0500

    worked on server side for modify recipe

--- dotnet/Capstone/Controllers/RecipeController.cs
+++ dotnet/Capstone/Controllers/RecipeController.cs
@@ -99,66 +99,6 @@ namespace Capstone.Controllers
                 dao.RemoveIngredientsFromRecipe(userRecipeId, ingredientId);
                 return NoContent();
 
-<<<<<<< HEAD
-=======
-namespace Capstone.Controllers
-{
-    [Route("[controller]")]
-    [ApiController]
-    public class RecipeController : ControllerBase
-    {
-
-        IRecipeDao dao;
-        IUserDao userDao;
-        public RecipeController(IRecipeDao RecipeDao, IUserDao UserDao)
-        {
-            this.dao = RecipeDao;
-            this.userDao = UserDao;
-        }
-
-
-        [HttpGet()]
-        public ActionResult<List<Recipe>> GetRecipes()
-        {
-            return Ok(dao.GetRecipes());
-        }
-
-        [HttpGet("{userId}")]
-        public ActionResult<List<Recipe>> GetRecipesByUserId(int userId)
-        {
-            return Ok(dao.GetRecipesByUserId(userId));
-        }
-
-
-        [HttpGet("{userId}/{recipeId}")]
-        public ActionResult<Recipe> GetUserRecipeById(int recipeId)
-        {
-            return Ok(dao.GetUserRecipeById(recipeId));
-        }
-
-        [HttpGet("public/{recipeName}")]
-        public ActionResult<Recipe> GetRecipeByName(string recipeName)
-        {
-            return Ok(dao.GetRecipeByName(recipeName));
-        }
-
-        [HttpGet("{userId}/{recipeId}/ingredients")]
-        public ActionResult<List<Ingredient>> GetIngredientsByRecipeId(int recipeId)
-        {
-            return Ok(dao.GetIngredientsByRecipeId(recipeId));
-        }
-
-
-        [HttpPost()]
-        public ActionResult<Recipe> CreateRecipe(Recipe newRecipe)
-        {
-            int currentUser = userDao.GetUserByUsername(User.Identity.Name).UserId;
-            Recipe result = dao.CreateRecipe(newRecipe, currentUser);
-
-            if (result.RecipeId == 0)
-            {
-                return BadRequest();
->>>>>>> b434f0d9a2d3e98b1ed007d7c33a9b252e524d7c
             }
             catch (DaoException ex)
             {
