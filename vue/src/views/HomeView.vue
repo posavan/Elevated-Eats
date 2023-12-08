819b28210c8263ea03a0c2a949b94e7642aba678
hamdidw <91859635+hamdidw@users.noreply.github.com>
2023-12-08 09:40:15 -0500

    added styling for login and home page

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,11 +1,87 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <!-- <p>You must be authenticated to see this</p> -->
+    <nav class="recipes">
+      <a href="/recipe">Recipes </a>
+    </nav>
+    <nav class="my-recipes">
+      <a href="/recipe/1">My Recipes</a>
+    </nav>
+    <nav class="meal-plans">
+      <a href="/mealPlans">My Meal Plans</a>
+    </nav>
   </div>
 </template>
 
 <script>
-export default {
-};
+export default {};
 </script>
+
+<style scoped>
+div.home {
+  background-color: rgb(120, 111, 84);
+  background-image: url("../../homeview.jpg");
+  color: wheat;
+  padding-top: 200px;
+  padding-bottom: 13%;
+  padding-top: 240px;
+  background-size: cover;
+  border-radius: 3%;
+  background-blend-mode: multiply;
+}
+h1 {
+  text-align: center;
+  font-size: 100px;
+}
+p {
+  text-align: center;
+}
+.recipes {
+  display: block;
+  color: wheat;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  background-color: rgb(213, 51, 51);
+  border-radius: 50px;
+  margin-left: 40%;
+  margin-right: 40%;
+  margin-bottom: 0.9%;
+  border-top: none;
+  border-left: none;
+  border-right: none;
+}
+.meal-plans {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  background-color: rgb(213, 51, 51);
+  border-radius: 50px;
+  margin-left: 40%;
+  margin-right: 40%;
+  margin-bottom: 0.9%;
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  border-bottom: none;
+}
+.my-recipes {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  background-color: rgb(213, 51, 51);
+  border-radius: 50px;
+  margin-left: 40%;
+  margin-right: 40%;
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  border-bottom: none;
+  margin-bottom: 0.9%;
+}
+</style>
--- vue/src/views/LoginView.vue
+++ vue/src/views/LoginView.vue
@@ -1,16 +1,23 @@
 <template>
   <div id="login">
     <form v-on:submit.prevent="login">
-      <h1 >Please Sign In</h1>
+      <h1>Sign In</h1>
       <div role="alert" v-if="invalidCredentials">
         Invalid username and password!
       </div>
+
       <div role="alert" v-if="this.$route.query.registration">
         Thank you for registering, please sign in.
       </div>
       <div class="form-input-group">
         <label for="username">Username</label>
-        <input type="text" id="username" v-model="user.username" required autofocus />
+        <input
+          type="text"
+          id="username"
+          v-model="user.username"
+          required
+          autofocus
+        />
       </div>
       <div class="form-input-group">
         <label for="password">Password</label>
@@ -18,7 +25,10 @@
       </div>
       <button type="submit">Sign in</button>
       <p>
-      <router-link v-bind:to="{ name: 'register' }">Need an account? Sign up.</router-link></p>
+        <router-link v-bind:to="{ name: 'register' }"
+          >Need an account? Sign up.</router-link
+        >
+      </p>
     </form>
   </div>
 </template>
@@ -32,39 +42,99 @@ export default {
     return {
       user: {
         username: "",
-        password: ""
+        password: "",
       },
-      invalidCredentials: false
+      invalidCredentials: false,
     };
   },
   methods: {
     login() {
       authService
         .login(this.user)
-        .then(response => {
+        .then((response) => {
           if (response.status == 200) {
             this.$store.commit("SET_AUTH_TOKEN", response.data.token);
             this.$store.commit("SET_USER", response.data.user);
             this.$router.push("/");
           }
         })
-        .catch(error => {
+        .catch((error) => {
           const response = error.response;
 
           if (response.status === 401) {
             this.invalidCredentials = true;
           }
         });
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style scoped>
+div#login {
+  background-color:rgb(186, 48, 48);
+  border-radius: 10%;
+  
+}
 .form-input-group {
   margin-bottom: 1rem;
 }
 label {
   margin-right: 0.5rem;
+}
+form {
+  padding-top: 15%;
+  background-color: wheat;
+  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+  font-size: large;
+  text-align: center;
+  border-radius: 50%;
+  padding: auto;
+  color:rgb(95, 25, 133);
+}
+button {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  background-color: brown;
+  border-radius: 50px;
+  margin-left: 48%;
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  border-bottom: none;
+}
+p {
+  margin: 20px;
+  padding-bottom: 15%;
+  padding-top: 1%;
+}
+input#username {
+  width: 10%;
+  height: 25px;
+  padding-top: 10px;
+  margin-bottom: 15px;
+  background: rgb(252, 235, 234);
+  border-bottom: 1px solid #fff;
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  text-align: center;
+  font-size: large;
+}
+input#password {
+  width: 10%;
+  height: 25px;
+  padding-top: 10px;
+  margin-bottom: 15px;
+  background: rgb(252, 235, 234);
+  border-bottom: 1px solid #fff;
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  text-align: center;
+  font-size: large;
 }
 </style>
